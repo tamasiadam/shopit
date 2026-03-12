95b94cd84ab9214d5286b60c74046ad13226dd74
Ádám <tamasi.adam.attila@diak.szbi-pg.hu>
2026-03-12 07:42:07 +0100

    bemutatásra kész

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("X");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function (e) {
-    e.stopPropagation();
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('#myUL');
 
