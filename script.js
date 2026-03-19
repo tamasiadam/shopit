d7d1592d86fea878b0f0408dd54d6617c2c0364b
Ádám <tamasi.adam.attila@diak.szbi-pg.hu>
2026-03-19 08:55:25 +0100

    fix

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('#myUL');
-
-list.addEventListener('click', function (ev) {
-  var li = ev.target.closest("li");
-  if (li && !ev.target.classList.contains("close")) {
-    li.classList.toggle("checked");
-  }
-});
-
-// Create a new list item when clicking on the "Add" button
+// hozzaadas
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
@@ -22,6 +12,8 @@ function newElement() {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
+
+// torles X
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("X");
@@ -37,3 +29,24 @@ function newElement() {
     }
   }
 } 
+
+// bezarja
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function (e) {
+    e.stopPropagation();
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// kipipálja
+var list = document.querySelector('#myUL');
+
+list.addEventListener('click', function (ev) {
+  var li = ev.target.closest("li");
+  if (li && !ev.target.classList.contains("close")) {
+    li.classList.toggle("checked");
+  }
+});
